6002cea7d6680db98c94e129355111990beb8dff
khalil <labiadkhalil11@gmail.com>
2023-09-08 03:00:26 +0100

    wxdfc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Entry point
  *
+ * Description: print a string
+ *
  * Return: Always 0 (Sucess)
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
