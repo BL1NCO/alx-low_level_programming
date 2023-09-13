6f5b9c3aa9c18432bae74b4181cc19889c1796f8
khalil <labiadkhalil11@gmail.com>
2023-09-13 03:49:07 +0100

    sdf

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,15 @@
 /**
  * _islower - printing from a to z lowercase
  *
+ * @c: The first integer define type of char
  * Return: Always 0 (Sucess)
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		return(1);
-		else  
-			return(0);
+		return (1);
+		else
+		return (0);
 
-	return(0);
+	return (0);
 }
