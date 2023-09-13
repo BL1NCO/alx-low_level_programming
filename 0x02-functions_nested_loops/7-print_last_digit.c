935f277034df011cd563874392b849f42a01bd8a
khalil <labiadkhalil11@gmail.com>
2023-09-13 18:59:32 +0100

    sdf

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @d: integer variable to return
+ */
+
+int print_last_digit(int d)
+{
+	return (d % 10);
+}
