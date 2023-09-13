281ce5512ac5930ebe97f7f545bf57e0472b8302
khalil <labiadkhalil11@gmail.com>
2023-09-13 02:48:34 +0100

    qsdf

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
-
-
-
-int main(void)
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0 (Sucess)
+ */
+void print_alphabet(void)
 {
 	char a = 'a';
 
 	for (; a <= 'z'; a++)
 		_putchar(a);
 	_putchar('\n');
-	return (0);
 }
