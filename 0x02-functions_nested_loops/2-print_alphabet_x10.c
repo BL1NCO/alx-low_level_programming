14c9ad23cddac5f6bff85ce1d84b25cf769a7592
khalil <labiadkhalil11@gmail.com>
2023-09-13 03:05:32 +0100

    dfg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0 (Sucess)
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int b = 1;
+
+	for (b = 1; b <= 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+		_putchar('\n');
+	}
+}
