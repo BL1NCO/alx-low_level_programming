3c318b49740c268c4ec4877a3dd40914ef54ce09
khalil <labiadkhalil11@gmail.com>
2023-09-14 01:06:23 +0100

    zdef

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if ((a * b) > 9)
+			{
+				_putchar('0' + ((a * b) / 10));
+				_putchar('0' + ((a * b) % 10));
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+					((a * b) <= 9);
+					_putchar('0' + (a * b));
+					_putchar(',');
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
+
