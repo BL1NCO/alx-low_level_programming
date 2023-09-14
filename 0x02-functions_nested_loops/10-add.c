592b8cfb63b6e078390b865b264ed69918aa8694
khalil <labiadkhalil11@gmail.com>
2023-09-14 02:29:40 +0100

    qsd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @c: integer variable to return
+ * @b: integer adds sum
+ */
+
+int add(int c, int b)
+{
+	return (c + b);
+}
