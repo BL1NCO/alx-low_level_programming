e6ba16a1ef8a22863a4532a6ae854d9f53042800
khalil <labiadkhalil11@gmail.com>
2023-09-16 00:26:58 +0100

    qsdf

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+}
