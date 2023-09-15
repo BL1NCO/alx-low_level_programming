50489af0d89875e98295104ab215721abe90f2d3
khalil <labiadkhalil11@gmail.com>
2023-09-16 00:06:03 +0100

    qsd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @a: parametre used as integer
+ * @b: parametre used as integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
