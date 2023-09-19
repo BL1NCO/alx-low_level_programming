0118dfc755124e0c70e2455ed5e5c7f89226e804
khalil <labiadkhalil11@gmail.com>
2023-09-19 14:44:20 +0100

    qdsf

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @n: parametre used as integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
