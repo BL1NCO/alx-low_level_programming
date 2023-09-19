1c99f71aadf6fe34deb2c4a783011fd2c95f3127
khalil <labiadkhalil11@gmail.com>
2023-09-19 16:54:51 +0100

    qsd

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @a: parametre used as integer
+ * @b: parametre used as integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int s;
+	s = *a;
+	*a = *b;
+	*b = s;
+}
