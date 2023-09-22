9142644c584ebcfd1b9435d6f66d191ad4ad710d
khalil <labiadkhalil11@gmail.com>
2023-09-22 23:30:34 +0100

    qsd

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int y;
+	int i;
+	int j;
 
-	for (y = 0; dest[y];)
-		y++;
-	int s;
-
-	for (s = 0; src[s]; s++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[y] = src[s];
-		y++;
+		i++;
 	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
+
