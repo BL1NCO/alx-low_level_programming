3c096b120fc4fa37df9e7b3ea7aca4829e2bacdc
khalil <labiadkhalil11@gmail.com>
2023-10-01 23:26:15 +0100

    qsd

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @n: parametre used as integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (a_sqrt_recursion(n, 0));
+}
+
+/**
+ * a_sqrt_recursion - recurses to find the natural
+ * @n: number integ
+ * @i: int
+ * Return: 0 succ
+ */
+
+int a_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (a_sqrt_recursion(n, i + 1));
+}
+
+
