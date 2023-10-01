06cee952dbf2a7b5e99efcd625e0d2313c992ab7
khalil <labiadkhalil11@gmail.com>
2023-10-01 23:12:04 +0100

    qsd

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @n: parametre used as integer
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		n *= factorial(n - 1);
+		return (n);
+	}
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+}
