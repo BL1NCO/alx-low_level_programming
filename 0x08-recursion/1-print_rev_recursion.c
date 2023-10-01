a34891a77c9335084e7219fc5156ed5f42417254
khalil <labiadkhalil11@gmail.com>
2023-10-01 22:02:56 +0100

    qsd

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - printing from a to z lowercase
+ * _puts_recursion - printing reversed string
  *
  * Return: Always 0 (Sucess)
  * @s: parametre used as integer
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	{
-		_puts_recursion(s + 1);
+		_puts_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
