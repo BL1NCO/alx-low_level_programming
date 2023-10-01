fd31f919d86bf44c0f4d0af5d2ba5bfc8e7814d0
khalil <labiadkhalil11@gmail.com>
2023-10-01 21:56:39 +0100

    qsd

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @s: parametre used as integer
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
