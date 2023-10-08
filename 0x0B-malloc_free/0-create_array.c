005f1518394aef3cf0491f561f2d400d2401841c
khalil <labiadkhalil11@gmail.com>
2023-10-08 12:44:51 +0100

    qsd

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_array - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @c: parametre used as integer
+ * @size: parametre used as integer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = (char *)malloc(size * sizeof(char));
+	int i;
+
+	if (size == 0 || ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
