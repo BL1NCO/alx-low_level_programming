b22d570adf1de19901ee95a75c82ba95061bd900
khalil <labiadkhalil11@gmail.com>
2023-10-08 13:28:31 +0100

    qsd

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strdup - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @str: parametre used as integer
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int j;
+	char *strstr;
+
+	strstr = str;
+	for (i = 0; *strstr != '\0'; strstr++)
+	{
+		i++;
+	}
+	char *ptr = (char *)malloc(i * sizeof(char));
+
+	if (ptr != NULL)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			ptr[j] = str[j];
+		}
+		return (ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
