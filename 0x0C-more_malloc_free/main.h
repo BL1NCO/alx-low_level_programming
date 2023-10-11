90413018a900484eccf27dd301ca89fdc40661cb
khalil <labiadkhalil11@gmail.com>
2023-10-11 22:10:35 +0100

    qsd

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MY_HEADER_FILE_H
 #define MY_HEADER_FILE_H
+
 #include <stdlib.h>
+#include <stdio.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
