07eb3ffc74a44f4e0b931955ca9ff5093967ddc0
khalil <labiadkhalil11@gmail.com>
2023-10-13 17:13:06 +0100

    qsd

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <limits.h>
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -12,6 +13,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
-int is_digit(char *s)
+int is_digit(char *s);
 
 #endif
