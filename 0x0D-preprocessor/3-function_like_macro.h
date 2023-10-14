b1edc5cc6ef42d75522bcd2740449595e9e77422
khalil <labiadkhalil11@gmail.com>
2023-10-14 21:45:03 +0100

    qsd

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#if (x < 0)
-#define ABS(x) ((x) * (-1))
-#elif (x > 0)
-#define ABS(x) (x)
-#endif
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 #endif
