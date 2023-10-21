e50d3be0e46e35c05f9d8dcbefe55e76ead56673
khalil <labiadkhalil11@gmail.com>
2023-10-21 22:23:22 +0100

    qsd

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * init_dog - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @d: parametre used as integer
+ * @name: parametre used as integer
+ * @age: parametre used as integer
+ * @owner: parametre used as integer
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
