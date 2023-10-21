e03714ae2511f6f46ca5bddabe1ec7a8e40b61bd
khalil <labiadkhalil11@gmail.com>
2023-10-21 22:27:52 +0100

    qsd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 /**
  * struct dog - structure
  * @name: char
@@ -14,7 +16,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
