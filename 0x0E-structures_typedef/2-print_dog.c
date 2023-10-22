d6613879437fbef5f9e5fc515e4621b5e99ba263
khalil <labiadkhalil11@gmail.com>
2023-10-22 14:11:35 +0100

    qsd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,12 +16,12 @@ void print_dog(struct dog *d)
 	{
 		d->name = "(nil)";
 	}
-	printf("Name: %s\n", d->name);
-	printf("Name: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		d->owner = "(nil)";
 	}
+	printf("Name: %s\n", d->name);
+	printf("Name: %f\n", d->age);
 	printf("Name: %s\n", d->owner);
 
 }
