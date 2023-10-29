afa6700bfe2b99fa4ccd062ed4841f5334ef6f7a
khalil <labiadkhalil11@gmail.com>
2023-10-29 10:22:54 +0100

    qsd

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - printing from a to z lowercase
+ *
+ * Return: Always 0 (Sucess)
+ * @name: parametre used as integer
+ * @f: parametre used as integer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(*name);
+}
+
