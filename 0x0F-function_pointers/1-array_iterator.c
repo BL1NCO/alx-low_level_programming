12b3da20dbbb07e13e3aab238a1b051354b53bdb
khalil <labiadkhalil11@gmail.com>
2023-10-29 18:40:25 +0100

    qsd

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,6 @@
 /**
  * array_iterator - printing from a to z lowercase
  *
- * Return: Always 0 (Sucess)
  * @array: parametre used as integer
  * @action: parametre used as integer
  * @size: parametre used as integer
@@ -17,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
 		 action(array[i]);
 	}
