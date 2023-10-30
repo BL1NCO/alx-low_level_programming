b5e463f5d77de3988a891f5e82f91a925890525e
khalil <labiadkhalil11@gmail.com>
2023-10-30 23:45:43 +0100

    sdf

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main fun
+ *
+ * @argc: num
+ * @argv: arg
+ * Return: Always 0 sucess
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int n1, n2;
+	char *opp;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	opp = argv[2];
+	n2 = atoi(argv[3]);
+
+
+	if (get_op_func(op) == NULL || opp[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*opp == '/' && n2 == 0) ||
+	    (*opp == '%' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(opp)(n1, n2));
+	return (0);
+	}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - printing from a to z lowercase
+ *
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - printing from a to z lowercase
+ *
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - printing from a to z lowercase
+ *
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - printing from a to z lowercase
+ *
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - printing from a to z lowercase
+ *
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
