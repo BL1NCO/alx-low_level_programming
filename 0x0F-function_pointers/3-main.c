057d65e028d868cf8ab0079d2bf6892ab8ab0eec
khalil <labiadkhalil11@gmail.com>
2023-10-30 23:52:49 +0100

    qsd

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	n2 = atoi(argv[3]);
 
 
-	if (get_op_func(op) == NULL || opp[1] != '\0')
+	if (get_op_func(opp) == NULL || opp[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
