ee94d093ac13e45889cdfb863702a99a005a6fc9
khalil <labiadkhalil11@gmail.com>
2023-10-31 01:06:00 +0100

    qsd

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,9 +15,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, s = 0;
 
 	va_start(a, n);
+
 	for (i = 0; i < n; i++)
 		s += va_arg(a, int);
 
 	va_end(a);
-	return (sum);
+	return (s);
 }
